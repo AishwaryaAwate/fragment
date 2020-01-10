079468e770248f18b0d235b269b0c944a3cbee3a
root <AishwaryaAwate>
2020-01-10 16:43:58 +0530

    Update

--- app/src/main/java/com/example/fragmenttask/ui/netwoking/NetworkingFragment.java
+++ app/src/main/java/com/example/fragmenttask/ui/netwoking/NetworkingFragment.java
@@ -3,13 +3,16 @@ package com.example.fragmenttask.ui.netwoking;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -45,7 +48,7 @@ import java.util.regex.Matcher;
 public class NetworkingFragment extends Fragment {
 
     TextInputEditText edt_username, edt_email, edt_password, edt_confirm_password, edt_mobileNo;
-    RadioGroup radioGroup,radio_male,radio_female;
+    RadioGroup radioGroup;
     Button btnSubmit;
     CircleImageView imgProfilePic;
     Uri imageUri;
@@ -63,7 +66,6 @@ public class NetworkingFragment extends Fragment {
 
         databaseHelper = new DatabaseHelper(getContext());
 
-
         edt_username = root.findViewById(R.id.edt_username);
         edt_email = root.findViewById(R.id.edt_email);
         edt_password = root.findViewById(R.id.edt_passward);
@@ -72,18 +74,16 @@ public class NetworkingFragment extends Fragment {
 
         radioGroup = root.findViewById(R.id.radioGroup);
 
-
-
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @SuppressLint("ResourceType")
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
 
-                if(checkedId == R.id.radio_female) {
+                if (checkedId == R.id.radio_female) {
                     gender = "Female";
                     Toast.makeText(getContext(), "Female", Toast.LENGTH_SHORT).show();
 
-                } else if(checkedId == R.id.radio_male) {
+                } else if (checkedId == R.id.radio_male) {
                     gender = "Male";
                     Toast.makeText(getContext(), "Male", Toast.LENGTH_SHORT).show();
 
@@ -100,19 +100,21 @@ public class NetworkingFragment extends Fragment {
             public void onClick(View v) {
 
 
+                openCameraIntent();
 
+/*              // This login will work for old version but not work on latest version more than android 7+
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 File photo = new File(Environment.getExternalStorageDirectory() ,"Pic.jpg");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-                imageUri = Uri.fromFile(photo);
+                */
+/*              intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+                imageUri = Uri.fromFile(photo);*//*
 
-                /*Uri apkURI = FileProvider.getUriForFile(getContext(),getContext().getPackageName() + ".provider", photo);
-                intent.setDataAndType(apkURI,null);
+
+                imageUri = FileProvider.getUriForFile(getContext(),getContext().getPackageName() + ".fileprovider", photo);
+                intent.setDataAndType(imageUri,null);
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-                 */
                 startActivityForResult(intent,100);
-
+*/
 
             }
         });
@@ -122,13 +124,10 @@ public class NetworkingFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
+                if (validateInputData()) {
 
-
-
-               if (validateInputData()) {
-
-                   RadioButton radioButton = (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
-                   Toast.makeText(getContext(), radioButton.getText(), Toast.LENGTH_SHORT).show();
+                    RadioButton radioButton = (RadioButton) radioGroup.findViewById(radioGroup.getCheckedRadioButtonId());
+                    Toast.makeText(getContext(), radioButton.getText(), Toast.LENGTH_SHORT).show();
 
                     Fragment fragment = new LoginFragment();
                     ((MainActivity) getContext()).replaceFragment(fragment, "Welcome page ");
@@ -136,19 +135,29 @@ public class NetworkingFragment extends Fragment {
                     insertIntoDB();
 
                 }
-
-
             }
         });
-
 
         return root;
     }
 
+    private void openCameraIntent() {
 
+
+        File photo = new File(Environment.getExternalStorageDirectory(), "temp.png");//"temp.png");
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            Uri photoURI = FileProvider.getUriForFile(getActivity(), getActivity().getApplicationContext().getPackageName(), photo);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        } else {
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        }
+        startActivityForResult(cameraIntent, 100);
+
+    }
 
     private void insertIntoDB() {
-
 
         NetworkPOJO networkPOJO = new NetworkPOJO(imageUri.toString(), edt_username.getText().toString(),
                 edt_email.getText().toString(),
@@ -158,20 +167,19 @@ public class NetworkingFragment extends Fragment {
                 gender
         );
 
-
         if (networkPOJO != null) {
 
             boolean isInserted = databaseHelper.insertData(networkPOJO);
             if (isInserted) {
 
                 Toast.makeText(getContext(), "User Added successfully", Toast.LENGTH_LONG).show();
-                Log.d("test", "Image Url :" +networkPOJO.getProfilePic_URL() +
-                                        " Username :" + networkPOJO.getUsername() +
-                                        " Email :" + networkPOJO.getEmail() +
-                                        " Password :" + networkPOJO.getPassword() +
-                                        " Confirm password :" + networkPOJO.getPassword() +
-                                        " Mobile :" + networkPOJO.getMobileNo()+
-                                        " Gender :" + networkPOJO.getGender());
+                Log.d("test", "Image Url :" + networkPOJO.getProfilePic_URL() +
+                        " Username :" + networkPOJO.getUsername() +
+                        " Email :" + networkPOJO.getEmail() +
+                        " Password :" + networkPOJO.getPassword() +
+                        " Confirm password :" + networkPOJO.getPassword() +
+                        " Mobile :" + networkPOJO.getMobileNo() +
+                        " Gender :" + networkPOJO.getGender());
             } else {
 
                 Toast.makeText(getContext(), "User not Added ", Toast.LENGTH_LONG).show();
@@ -183,6 +191,30 @@ public class NetworkingFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+        if (requestCode == 100) {
+
+            Bitmap bitmap = null;
+
+            File selected = new File(Environment.getExternalStorageDirectory(), "temp.png");
+//            Log.e("requestCode", "" + CODE);
+            bitmap = BitmapFactory.decodeFile(selected.getAbsolutePath());
+//            Log.e("requestCode", "" + CODE);
+//                bitmap = Bitmap.createScaledBitmap(bitmap, (int) (bitmap.getWidth(),0.4), (int) (bitmap.getHeight()  0.4), true);
+//            Log.e("requestCode", "" + CODE);
+            String rpath = getRealPathFromURI(getActivity(), getImageUri(getActivity(), bitmap), requestCode + "");
+
+
+            if (rpath != null) {
+
+                imgProfilePic.setImageBitmap(bitmap);
+            }
+
+
+        }
+
+/*
+
         switch (requestCode){
 
             case 100:
@@ -192,6 +224,16 @@ public class NetworkingFragment extends Fragment {
                     getActivity().getContentResolver().notifyChange(selectedImage,null);
                     ContentResolver cr = getActivity().getContentResolver();
 
+                    Bitmap bitmap=null;
+
+                    File selected = new File(Environment.getExternalStorageDirectory(), "temp.png");
+//            Log.e("requestCode", "" + CODE);
+                    bitmap = BitmapFactory.decodeFile(selected.getAbsolutePath());
+//            Log.e("requestCode", "" + CODE);
+                    bitmap = Bitmap.createScaledBitmap(bitmap, (int) (bitmap.getWidth()  0.4), (int) (bitmap.getHeight()  0.4), true);
+//            Log.e("requestCode", "" + CODE);
+                    String rpath = getRealPathFromURI(getActivity(), getImageUri(getActivity(), bitmap), requestCode + "");
+*//*
                     Bitmap bitmap;
 
                     try {
@@ -204,44 +246,71 @@ public class NetworkingFragment extends Fragment {
 
                         Toast.makeText(getActivity(), "Failed to load", Toast.LENGTH_SHORT).show();
                         Log.e("Camera", e.toString());
-                    }
+                    }*//*
+            }
+        }*/
+    }
 
-
+    public static String getRealPathFromURI(Context context, Uri contentUri, String name) {
+        Cursor cursor = null;
+        try {
+            //Log.e("name", name);
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String path = cursor.getString(column_index);
+            //Log.e("path", path);
+            File dir = new File(Environment.getExternalStorageDirectory() + "/profileTempImg");
+            if (!dir.exists())
+                dir.mkdirs();
+            String rpath = Environment.getExternalStorageDirectory() + "/profileTempImg/" + (name.contains("sign") ? name : "evidence" + name) + ".jpg";
+            //Log.e("rpath", rpath);
+            new File(path).renameTo(new File(rpath));
+            return rpath;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
     }
 
-       private boolean validateInputData() {
+    public static Uri getImageUri(Context context, Bitmap inImage) {
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "profilePic", null);
+        //Log.e("path", path);
+        return Uri.parse(path);
+    }
 
-        if (edt_username.getText().toString().length()==0) {
+    private boolean validateInputData() {
+
+        if (edt_username.getText().toString().length() == 0) {
             edt_username.requestFocus();
             edt_username.setError("Enter valid Username");
             return false;
         }
-         if (!edt_email.getText().toString().matches(EMAIL_PATTERN)) {
-             edt_email.requestFocus();
-             edt_email.setError("Enter valid Email");
-             return false;
-         }
-          if (!edt_password.getText().toString().matches(PASSWORD_PATTERN)) {
-              edt_password.requestFocus();
-              edt_password.setError("Enter valid Password");
-              return false;
-          }
+        if (!edt_email.getText().toString().matches(EMAIL_PATTERN)) {
+            edt_email.requestFocus();
+            edt_email.setError("Enter valid Email");
+            return false;
+        }
+        if (!edt_password.getText().toString().matches(PASSWORD_PATTERN)) {
+            edt_password.requestFocus();
+            edt_password.setError("Enter valid Password");
+            return false;
+        }
 
-         if (!edt_password.getText().toString().equals(edt_confirm_password.getText().toString())) {
-             edt_confirm_password.requestFocus();
-             edt_confirm_password.setError("Password do not match");
-             return false;
-         }
+        if (!edt_password.getText().toString().equals(edt_confirm_password.getText().toString())) {
+            edt_confirm_password.requestFocus();
+            edt_confirm_password.setError("Password do not match");
+            return false;
+        }
 
-         if (edt_mobileNo.getText().toString().length() != 10) {
-                 edt_mobileNo.requestFocus();
-                 edt_mobileNo.setError("Enter valid mobile number");
-                 return false;
-         }
+        if (edt_mobileNo.getText().toString().length() != 10) {
+            edt_mobileNo.requestFocus();
+            edt_mobileNo.setError("Enter valid mobile number");
+            return false;
+        }
 
-
-         return true;
+        return true;
     }
 }
--- app/src/main/java/com/example/fragmenttask/ui/php/PHPFragment.java
+++ app/src/main/java/com/example/fragmenttask/ui/php/PHPFragment.java
@@ -1,10 +1,13 @@
 package com.example.fragmenttask.ui.php;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,23 +16,19 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.fragmenttask.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
 
 public class PHPFragment extends Fragment {
 
-    private PHPViewModel phpViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        phpViewModel =
-                ViewModelProviders.of(this).get(PHPViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_php, container, false);
-        final TextView textView = root.findViewById(R.id.text_php);
-        phpViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+     View root = inflater.inflate(R.layout.fragment_php, container, false);
+
+
+     return root;
     }
 }
